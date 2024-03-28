ad6886cdf781a6466bf793a33262bfd74ecedb3e
Fred Trelz <10674853+ftrelz@users.noreply.github.com>
2024-03-28 13:17:53 -0500

    lsp setup improvements

--- after/plugin/lsp-zero.lua
+++ after/plugin/lsp-zero.lua
@@ -1,25 +1,26 @@
 local lsp = require('lsp-zero').preset("recommended")
 
-lsp.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
-  lsp.default_keymaps({buffer = bufnr})
+lsp.on_attach(function(_, bufnr)
+    -- see :help lsp-zero-keybindings
+    -- to learn the available actions
+    lsp.default_keymaps({ buffer = bufnr })
 end)
 
 lsp.ensure_installed({
-	"clangd",
-	"cssls",
-	"eslint",
-	"html",
-	"jsonls",
+    "clangd",
+    "cssls",
+    "eslint",
+    "html",
+    "jsonls",
     "tailwindcss",
     "tsserver",
-	"pylsp",
+    "pylsp",
     "emmet_language_server"
 })
 
 -- (Optional) Configure lua language server for neovim
-require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls({
+local lspconfig = require("lspconfig")
+lspconfig.lua_ls.setup(lsp.nvim_lua_ls({
     settings = {
         Lua = {
             completion = {
@@ -28,7 +29,36 @@ require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls({
         }
     }
 }))
-require('lspconfig').tsserver.setup({
+
+lspconfig.tsserver.setup({
+    keys = {
+        {
+            "<leader>co",
+            function()
+                vim.lsp.buf.code_action({
+                    apply = true,
+                    context = {
+                        only = { "source.organizeImports.ts" },
+                        diagnostics = {},
+                    },
+                })
+            end,
+            desc = "Organize Imports",
+        },
+        {
+            "<leader>cR",
+            function()
+                vim.lsp.buf.code_action({
+                    apply = true,
+                    context = {
+                        only = { "source.removeUnused.ts" },
+                        diagnostics = {},
+                    },
+                })
+            end,
+            desc = "Remove Unused Imports",
+        },
+    },
     settings = {
         typescript = {
             format = {
@@ -49,12 +79,67 @@ require('lspconfig').tsserver.setup({
         },
     },
 })
-require('lspconfig').emmet_language_server.setup({
+
+lspconfig.tailwindcss.setup({})
+
+lspconfig.emmet_language_server.setup({
     init_options = {
         syntax_profiles = {
             jsx = "xhtml"
         }
     }
+})
+
+lspconfig.jsonls.setup({
+    -- lazy-load schemastore when needed
+    on_new_config = function(new_config)
+        new_config.settings.json.schemas = new_config.settings.json.schemas or {}
+        vim.list_extend(new_config.settings.json.schemas, require("schemastore").json.schemas())
+    end,
+    settings = {
+        json = {
+            format = {
+                enable = true,
+            },
+            validate = { enable = true },
+        },
+    },
+})
+
+lspconfig.clangd.setup({
+    keys = {
+        { "<leader>cR", "<cmd>ClangdSwitchSourceHeader<cr>", desc = "Switch Source/Header (C/C++)" },
+    },
+    root_dir = function(fname)
+        return require("lspconfig.util").root_pattern(
+            "Makefile",
+            "configure.ac",
+            "configure.in",
+            "config.h.in",
+            "meson.build",
+            "meson_options.txt",
+            "build.ninja"
+        )(fname) or require("lspconfig.util").root_pattern("compile_commands.json", "compile_flags.txt")(
+            fname
+        ) or require("lspconfig.util").find_git_ancestor(fname)
+    end,
+    capabilities = {
+        offsetEncoding = { "utf-16" },
+    },
+    cmd = {
+        "clangd",
+        "--background-index",
+        "--clang-tidy",
+        "--header-insertion=iwyu",
+        "--completion-style=detailed",
+        "--function-arg-placeholders",
+        "--fallback-style=llvm",
+    },
+    init_options = {
+        usePlaceholders = true,
+        completeUnimported = true,
+        clangdFileStatus = true,
+    },
 })
 
 lsp.setup()
@@ -67,4 +152,3 @@ lsp.format_on_save({
         ["jsonls"] = { "json" }
     }
 })
-
